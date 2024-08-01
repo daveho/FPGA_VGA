f66c0a57d46634e89a0321daf60d35ff6362d0af
David Hovemeyer <david.hovemeyer@gmail.com>
2024-08-01 08:28:41 -0400

    implemented test bench for hcount module

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@ TOOLS = $(HOME)/.apio/packages/tools-oss-cad-suite/bin
 
 .PHONY: all test_hcount
 
+all :
+	@echo "Use 'test_XXX' target to run the test for the XXX module"
+	@echo "E.g., 'make test_hcount'"
+
 hcount_tb.vvp : hcount_tb.v hcount.v
 	$(TOOLS)/iverilog -o $@ $^
 
--- hcount_tb.v
+++ hcount_tb.v
@@ -10,6 +10,13 @@
 // recommend this
 `default_nettype none
 
+// Test assertion macro (stolen from John Winans's tutorials)
+`define ASSERT(cond) \
+  if ( ~(cond) ) begin \
+    $display( "s:%0d %m time:%5t ASSERTION FAILED: cond", `__FILE__, `__LINE__, $time ); \
+    $finish; \
+  end
+
 module hcount_tb();
 
   // The testbench just needs to control -RST and CLK
@@ -29,8 +36,46 @@ module hcount_tb();
                           .hEndActive( hEndActive ) );
 
   initial begin
+    // generate dump file we can inspect using gtkwave
+    $dumpfile( "hcount_tb.vcd" );
+    $dumpvars;
+  end
+
+  integer i; // loop counter
+
+  initial begin
+    // generate a reset pulse
     nrst = 0;
     clk = 0;
+    #1;
+    clk = 1;
+    #1;
+    nrst = 1;
+    clk = 0;
+    #1;
+
+    // module is now out of reset
+
+    // simulate 800 cycles (one scanline), checking outputs
+    // to make sure they're right
+    for ( i = 0; i < 800; i++) begin
+      // Check that output values are correct
+      `ASSERT( hCountEnd == ( i == 799 ) );
+      `ASSERT( hBeginPulse == ( i == 655 ) );
+      `ASSERT( hEndPulse == ( i == 751 ) );
+      `ASSERT( hVisEnd == ( i == 639 ) );
+      `ASSERT( hBeginActive == ( i == 793 ) );
+      `ASSERT( hEndActive == ( i == 633 ) );
+
+      // Generate one clock pulse
+      clk = 1;
+      #1;
+      clk = 0;
+      #1;
+    end
+
+    // If we got here, all of the test assertions succeeded
+    $display( "All tests passed!" );
   end
 
 endmodule
