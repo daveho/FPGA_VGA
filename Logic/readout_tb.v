bcc37d7091c029ea22e267dc7388786ce3287b6c
David Hovemeyer <david.hovemeyer@gmail.com>
2024-08-02 20:25:33 -0400

    observe readoutAddr through first 17 pixel rows
    This gets us to the first pixel row of the second
    character row, where the row begin address will have
    been updated to reflect the start in VRAM of the
    characters and attributes in the second character
    row.
    
    This is reasonably good evidence that the readout
    logic is ok.

--- Logic/readout_tb.v
+++ Logic/readout_tb.v
@@ -93,6 +93,9 @@ module readout_tb();
                             // Outputs
                             .readoutAddr( readoutAddr ) );
 
+  // loop counter for tests
+  integer i;
+
   initial begin
     // set tick count to 0
     ticks = 0;
@@ -109,7 +112,7 @@ module readout_tb();
     while ( ~vEndPulse ) begin
       `TICK( clk );
     end
-    $display( "v end pulse at vcount %d, ticks=%d", vCount, ticks );
+    //$display( "v end pulse at vcount %d, ticks=%d", vCount, ticks );
     `ASSERT( readoutAddr == 13'd0 );
 
     // advance until vActive and hBeginActive are both asserted.
@@ -120,7 +123,12 @@ module readout_tb();
     end
     `ASSERT( vActive );
     `ASSERT( hBeginActive );
-    $display( "begin activity, vCount=%d, ticks=%d", vCount, ticks );
+    //$display( "begin activity, vCount=%d, ticks=%d", vCount, ticks );
+
+    // vCount should be 0 at this point: vCount is incremented
+    // at hEndPulse, to allow vCount to be correct when activity
+    // starts for the scanline that is about to be displayed)
+    `ASSERT( vCount == 12'd0 );
 
     // Readout address should still be 0 (this is the first character
     // being fetched from VRAM)
@@ -137,6 +145,84 @@ module readout_tb();
     `ASSERT( readoutAddr == 13'd0 );
     `TICK( clk );
     `ASSERT( readoutAddr == 13'd1 );
+
+    // advance to the end of the frame (which is only a few cycles away)
+    while ( ~(hCountEnd & vCountZero) ) begin
+      `TICK( clk );
+    end
+
+    // advance to the end of the visible part of the first scanline;
+    // readoutAddr should be 160
+    `GENCLOCK( 640, clk );
+    `ASSERT( readoutAddr == 13'd160 );
+
+    // advance to the end of the horizontal sync pulse
+    while ( ~hEndPulse ) begin
+      `TICK( clk );
+    end
+
+    // readoutAddr should have been reset back to 0
+    //$display( "end of first scanline, readoutAddr=%d, ticks=%d", readoutAddr, ticks );
+    `ASSERT( readoutAddr == 13'd0 );
+
+    // advance to the end of the scanline
+    while ( ~hCountEnd ) begin
+      `TICK( clk );
+    end
+
+    // advance through 14 more scanlines: these are all part of the first row of
+    // characters, so for each, at the end of of the visible horizontal region,
+    // readoutAddr should be 160, and at the end of the hsync pulse, readoutAddr
+    // should have been set back to 0
+    for ( i = 0; i < 14; i++ ) begin
+      // advance to end of horizontal visibility
+      while ( ~hVisEnd ) begin
+        `TICK( clk );
+      end
+
+      // readoutAddr should be 160
+      `ASSERT( readoutAddr == 13'd160 );
+
+      // advance to end of hsync pulse
+      while ( ~hEndPulse ) begin
+        `TICK( clk );
+      end
+
+      // readoutAddr should have been reset back to 0
+      `ASSERT( readoutAddr == 13'd0 );
+    end
+
+    // last row of pixels in the first character row: readoutAddr should be be
+    // 160 at the end of the horizontal visible region, but it should REMAIN 160
+    // at the end of the hsync pulse
+    while ( ~hVisEnd ) begin
+      `TICK( clk );
+    end
+    `ASSERT( readoutAddr == 13'd160 );
+    while ( ~hEndPulse ) begin
+      `TICK( clk );
+    end
+    `ASSERT( readoutAddr == 13'd160 );
+
+    // advance to end of scanline
+    while ( ~hCountEnd ) begin
+      `TICK( clk );
+    end
+
+    // advance to end of horizontal visible region: this is the first row of pixels
+    // in the second character row, so after fetching all of the characters and attributes,
+    // readoutAddr should be 320
+    while ( ~hVisEnd ) begin
+      `TICK( clk );
+    end
+    `ASSERT( readoutAddr == 13'd320 );
+
+    // advance to end of hsync pulse: readoutAddr should have been reset back to
+    // the new row begin address, 160
+    while ( ~hEndPulse ) begin
+      `TICK( clk );
+    end
+    `ASSERT( readoutAddr == 13'd160 );
 
     $display( "All tests passed!" );
   end
