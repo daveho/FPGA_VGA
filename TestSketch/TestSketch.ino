6072db55a710847ab8bfeb29a5e6ca1eaa5777f8
David Hovemeyer <david.hovemeyer@gmail.com>
2025-11-25 16:19:18 -0500

    test readback: it seems to work!

--- TestSketch/TestSketch.ino
+++ TestSketch/TestSketch.ino
@@ -14,13 +14,19 @@
 //   Arduino D11: output to display -R (read strobe)
 //   Arduino D12: output to display -W (write strobe)
 //   Arduino D13: output to display -RST (reset)
+//   Arduino D14: pushbutton input (0=pressed), used to start the tests
+//   Arduino D15: output to green LED (if tests pass)
+//   Arduino D16: output to red LED (if tests fail)
 
-#define PIN_SER_DATA 8
-#define PIN_SER_CLK  9
-#define PIN_SER_RCLK 10
-#define PIN_NR       11
-#define PIN_NW       12
-#define PIN_NRST     13
+#define PIN_SER_DATA  8
+#define PIN_SER_CLK   9
+#define PIN_SER_RCLK  10
+#define PIN_NR        11
+#define PIN_NW        12
+#define PIN_NRST      13
+#define PIN_BTN       14
+#define PIN_GREEN_LED 15
+#define PIN_RED_LED   16
 
 // Set the display address.
 void setAddr( uint16_t addr ) {
@@ -42,9 +48,26 @@ void setAddr( uint16_t addr ) {
 
 void writeVRAM( uint16_t addr, uint8_t byte ) {
   setAddr( addr );
+  DDRD = 0xFF; // set port D pins to output
   PORTD = byte;
   digitalWrite( PIN_NW, LOW );
   digitalWrite( PIN_NW, HIGH );
+}
+
+uint8_t readVRAM( uint16_t addr ) {
+  setAddr( addr );
+  DDRD = 0x00;  // set port D pins to input
+  PORTD = 0x00; // no pull-ups
+  uint8_t data;
+  digitalWrite( PIN_NR, LOW ); // start read pulse
+  data = PIND; // read byte from data bus
+  digitalWrite( PIN_NR, HIGH ); // end read pulse
+
+  // set PORTD pins back to output:
+  // avoids leaving the bus transceiver A-side inputs floating
+  DDRD = 0xFF;
+
+  return data;
 }
 
 #define BLACK   0
@@ -56,7 +79,7 @@ void writeVRAM( uint16_t addr, uint8_t byte ) {
 #define CYAN    6
 #define GRAY    7
 #define INTENSE 8
-#define ATTR( fg, bg ) (bg|(fg << 4 ))
+#define ATTR( fg, bg ) ((bg)|((fg) << 4 ))
 
 // Display text, starting at specified VRAM address
 void displayText( uint16_t addr, const char *s, uint8_t attr ) {
@@ -79,11 +102,74 @@ void clearDisplay() {
   }
 }
 
+#define HELLO_TEXT_ADDR 2314
+
+// Check the result of clearing the display:
+// every character should be set to space, and every
+// attribute should be set to fg=GRAY bg=BLACK.
+bool checkDisplayClear( bool good ) {
+  uint16_t addr = 0;
+  uint8_t val;
+  for ( int i = 0; i < 2400; ++i ) {
+    val = readVRAM( addr++ );
+    if ( val != ' ' )
+      good = false;
+    val = readVRAM( addr++ );
+    if ( val != ATTR(GRAY,BLACK) )
+      good = false;
+  }
+
+  return good;
+}
+
+// Check that the "Hello!" text was written correctly.
+//
+bool checkHelloText( bool good ) {
+  const char expected[] = "Hello!";
+  uint16_t addr = HELLO_TEXT_ADDR;
+  uint8_t val;
+  for ( int i = 0; i < 6; ++i ) {
+    val = readVRAM( addr++ );
+    if ( val != expected[i] )
+      good = false;
+    val = readVRAM( addr++ );
+    if ( val != ATTR(YELLOW|INTENSE, BLUE) )
+      good = false;
+  }
+  return good;
+}
+
+void runTests() {
+  // turn off green and red LEDs
+  digitalWrite( PIN_GREEN_LED, LOW );
+  digitalWrite( PIN_RED_LED, LOW );
+
+  // Generate a reset pulse for the display controller
+  digitalWrite( PIN_NRST, LOW );
+  delay( 1 );
+  digitalWrite( PIN_NRST, HIGH );
+
+  bool good = true;
+
+  clearDisplay();
+  good = checkDisplayClear( good );
+  displayText( 2314, "Hello!", ATTR(YELLOW|INTENSE, BLUE) );
+  good = checkHelloText( good );
+
+  // Turn on green or red LED
+  digitalWrite( good ? PIN_GREEN_LED : PIN_RED_LED, HIGH );
+
+  // wait for a bit
+  delay( 10 );
+}
+
 void setup() {
   // Initial state
 
   // PORTD configured as output
   // D8-D13 configured as output
+  // D14 configured as input with pullup
+  // D15-D16 configured as output
 
   DDRD = 0xFF;
   pinMode( PIN_SER_DATA, OUTPUT );
@@ -92,6 +178,9 @@ void setup() {
   pinMode( PIN_NR, OUTPUT );
   pinMode( PIN_NW, OUTPUT );
   pinMode( PIN_NRST, OUTPUT );
+  pinMode( PIN_BTN, INPUT_PULLUP );
+  pinMode( PIN_GREEN_LED, OUTPUT );
+  pinMode( PIN_RED_LED, OUTPUT );
 
   // Drive serial data and register clocks low
   digitalWrite( PIN_SER_CLK, LOW );
@@ -102,19 +191,19 @@ void setup() {
   digitalWrite( PIN_NW, HIGH );
   digitalWrite( PIN_NRST, HIGH );
 
+  // Turn off LEDs
+  digitalWrite( PIN_GREEN_LED, LOW );
+  digitalWrite( PIN_RED_LED, LOW );
+
   // Wait for a bit
   delay( 2 );
-
-  // Generate a reset pulse for the display controller
-  digitalWrite( PIN_NRST, LOW );
-  delay( 1 );
-  digitalWrite( PIN_NRST, HIGH );
-
-  clearDisplay();
-  displayText( 2314, "Hello!", ATTR(GRAY, BLACK) );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for (;;) {
+    if ( digitalRead( PIN_BTN ) == LOW ) {
+      // button pressed, so run the tests
+      runTests();
+    }
+  }
 }
